39e9905e16113ca648c01ffba24394dea9e73aec
Shannon Mulloy <smulloy21@gmail.com>
2015-09-11 16:54:28 -0700

    edit styling

--- js/scripts.js
+++ js/scripts.js
@@ -9,13 +9,14 @@ var replaceWord = function(string, find, replace) {
 var madlibs = {
   madlib1: "Hello Noun1!",
   madlib2: "I Verb1 Noun(sing)1. Noun(sing)1 is the best. Noun(sing)1 likes Adjective1 Noun(plural)1.",
-  madlib3: "Name1 is a Noun(sing)1. Name1 likes to Verb1 Noun(plural)2."
+  madlib3: "Name1 is a Noun(sing)1. Name1 likes to Verb1 Noun(plural)2.",
+  madlib4: "Twas Adjective1, and the Adjective2 Noun(plural)1, Did Verb1 and Verb2 in the Noun1, All Adjective3 were the Noun(plural)2, And the Adjective4 Noun(plural)3 Adjective5."
 };
 
 var findKeywords = function(string) {
   var output = [];
   var arr = string.replace(/[.,!?:;'"-]+/g, '').split(' ');
-  var partsOfSpeech = ["Noun", "Noun(sing)", "Noun(plural)", "Verb", "Verb(3rdSing)", "Adjective", "Name"];
+  var partsOfSpeech = ["Noun", "Noun(sing)", "Noun(plural)", "Verb", "Verb(3rdSing)", "Verb(progressive)", "Verb(past)", "Adjective", "Name"];
   for(var i = 0; i < arr.length; i++) {
     for(var j = 0; j < partsOfSpeech.length; j++) {
       for(var k = 0; k < arr.length; k++) {
@@ -42,7 +43,7 @@ $(document).ready(function(){
       arr[i] = arr[i].substring(0, arr[i].length - 1);
       $('form#keywords-form').append('<input id="' + i + '" placeholder="' + arr[i] +'" required>');
     }
-    $('form#keywords-form').append('<button type="submit" class="btn btn-primary">Go Mad!</button>');
+    $('form#keywords-form').append('<br><br><button type="submit" class="btn btn-mad btn-primary">Go Mad!</button>');
     $('#keywords').show();
     event.preventDefault();
   });
